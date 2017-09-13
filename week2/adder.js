f06764d13f8afd88a4790259c757b356440cdc93
Cam Crews <camcrews@gmail.com>
2017-09-13 15:45:24 -0400

    adder example

--- week2/adder.js
+++ week2/adder.js
@@ -2,10 +2,12 @@ document.addEventListener("DOMContentLoaded",function(){
   // stuff we do after load, lives in here....
   document.getElementById("solve").addEventListener("click",function(){
     // stuff we do after a click is detected on #solve
-    document.getElementById("num1").value
+    var num1 = document.getElementById("num1").value;
     // grab the value from num1
     //        and num2
+    var num2 = document.getElementById("num2").value;
     // add them together (after converting them to numbers)
     // and update the innerText of #result
+    document.getElementById("result").innerText = parseInt(num1) + parseInt(num2);
   });
 });
