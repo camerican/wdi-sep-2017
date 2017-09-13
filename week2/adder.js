b272a3384ba4ad14d156229c0cbe7477b2bcf774
Cam Crews <camcrews@gmail.com>
2017-09-13 13:55:54 -0400

    Adder example

---
+++ week2/adder.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded",function(){
+  // stuff we do after load, lives in here....
+  document.getElementById("solve").addEventListener("click",function(){
+    // stuff we do after a click is detected on #solve
+    document.getElementById("num1").value
+    // grab the value from num1
+    //        and num2
+    // add them together (after converting them to numbers)
+    // and update the innerText of #result
+  });
+});
