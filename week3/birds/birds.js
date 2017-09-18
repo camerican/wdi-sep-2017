70c7ec84aa5a006797813a0c3ff9137593585a71
Cam Crews <camcrews@gmail.com>
2017-09-18 12:32:52 -0400

    Birds example

---
+++ week3/birds/birds.js
@@ -0,0 +1,92 @@
+const birds = [
+  {
+    name: "Black-bellied Whistling-Duck",
+    species: "Dendrocygna autumnalis",
+    order: "Anseriformes",
+    family: "Anatidae",
+    image: "https://download.ams.birds.cornell.edu/api/v1/asset/40180501/medium"
+  },
+  {
+    name: "Elf Owl",
+    species: "Micrathene whitneyi",
+    order: "Strigiformes",
+    family: "Strigidae",
+    image: "https://download.ams.birds.cornell.edu/api/v1/asset/24921511/1200"
+  },
+  {
+    name: "Masked Booby",
+    species: "Sula dactylatra",
+    order: "Suliformes",
+    family: "Sulidae",
+    image: "https://download.ams.birds.cornell.edu/api/v1/asset/24916161/1200"
+  },
+  {
+    name: "Cape May Warbler",
+    species: "Setophaga tigrina",
+    order: "Passeriformes",
+    family: "Parulidae",
+    image: "https://download.ams.birds.cornell.edu/api/v1/asset/24912541/1200"
+  }
+];
+
+// Constructor for birds...
+
+function Bird( name, species, order, family, image ) {
+  this.name = name;
+  this.species = species;
+  this.order = order;
+  this.family = family;
+  this.image = image;
+}
+
+let warbler = new Bird(
+  "Cape May Warbler",
+  "Setophaga tigrina",
+  "Passeriformes",
+  "Parulidae",
+  "https://download.ams.birds.cornell.edu/api/v1/asset/24912541/1200"
+);
+
+let bird2 = new Bird(
+ "Masked Booby",
+ "Sula dactylatra",
+ "Suliformes",
+ "Sulidae",
+ "https://download.ams.birds.cornell.edu/api/v1/asset/24916161/1200"
+);
+
+const birds2 = [
+  new Bird(
+    "Cape May Warbler",
+    "Setophaga tigrina",
+    "Passeriformes",
+    "Parulidae",
+    "https://download.ams.birds.cornell.edu/api/v1/asset/24912541/1200"
+  ),
+  new Bird(
+   "Masked Booby",
+   "Sula dactylatra",
+   "Suliformes",
+   "Sulidae",
+   "https://download.ams.birds.cornell.edu/api/v1/asset/24916161/1200"
+  )
+];
+
+
+// 
+
+
+
+// if( true ) {
+//   var madeByVar = 1;
+//   let madeByLet = 2;
+// }
+// console.log(madeByVar); //1
+// console.log(madeByLet); // ERROR
+
+
+
+
+
+
+
---
+++ week3/birds/lesson.js
@@ -0,0 +1,50 @@
+// Maybe we want to build a Lesson object
+// that has a type and a name...
+function Lesson( name, type ) {
+  this.name = name;
+  this.type = type || "lecture";
+  /*
+   * Determines the number of expected minutes for a
+   * lesson instance...
+   */
+  this.expectedDuration = function(){
+    if( this.type == "workshop" ) {
+      return 180;
+    } else {
+      return 60;
+    }
+  }
+}
+
+const lessons = [
+  new Lesson( "Object Oriented JavaScript" ),
+  new Lesson( "Object Oriented JavaScript Problems", "workshop")
+];
+// and then maybe each lesson has an expected duration
+// with lectures taking 1hr, and workshops taking 3hr
+
+
+// { 
+//   monday: [
+//     {type: "lecture",
+//      name: "Object Oriented JavaScript"},
+//     {type: "workshop",
+//      name: "Object Oriented JavaScript Problems"}
+//   ],
+//   tuesday: [
+//     {type: "workshop",
+//      name: "Object Oriented JavaScript Problems"}
+//   ],
+//   wednesday: [
+//     {type: "workshop",
+//      name: "Jukebox Starter Kit"
+//     }
+//   ],
+//   thursday: null,
+//   friday: [
+//     {type: "lecture",
+//      name: "jQuery Basics"},
+//     {type: "lecture",
+//      name: "APIs"}
+//   ]
+// }
