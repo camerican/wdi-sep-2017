26151a67d4df4e3835a23638d4974255eb8b5ebd
Cam Crews <camcrews@gmail.com>
2017-09-18 18:00:22 -0400

    Team example

--- week3/team/main.js
+++ week3/team/main.js
@@ -15,3 +15,76 @@
 // A Player will be another object type with a name, number (shirt number), and status (healthy - true, or injured - false)
 
 // We will have a setHealthy and setInjured status to alter their status.
+
+// Team constructor
+
+// function Team( options ) {
+//   this.name = options.name;
+//   this.sport = options.sport;
+//   this.playerCount = options.playerCount
+//   this.roster = [];
+// }
+
+function Team( name, sport, playerCount ) {
+  this.name = name;
+  this.sport = sport;
+  this.playerCount = playerCount;
+  this.roster = []; // we'll put our players in here
+}
+// allow us to add players
+Team.prototype.addPlayer = function( player ) {
+  // to do: make sure it's actually a Player before adding
+  this.roster.push(player);
+}
+Team.prototype.smartAddPlayer = function( name, number ) {
+  this.roster.push( new Player( name, number) );
+}
+
+function SoccerTeam( name, league, sponsor ) {
+  this.league = league;
+  this.sponsor = sponsor;
+  Team.call(this, name, "Soccer", 11);
+  // Team.apply(this, Array.from(arguments).slice(0,3));
+}
+SoccerTeam.prototype = Object.create(Team.prototype);
+
+function Player( name, number ) {
+  this.name = name;
+  this.number = number;
+  this.status = true;
+}
+Player.prototype.setHealthy = function() {
+  this.status = true;
+}
+Player.prototype.setInjured = function(){
+  this.status = false;
+}
+
+
+let mets = new Team("Mets", "baseball", 9);
+let chelsea = new SoccerTeam("Chelsea","Premier League", "Yokohama Tyres");
+
+chelsea.addPlayer( new Player( "Eden Hazzard", 10 ) );
+let player = new Player( "Alvara Morata", 9 );
+chelsea.addPlayer( player );
+
+chelsea.smartAddPlayer( "Thibolt Courtois", 1 );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
