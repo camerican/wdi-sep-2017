abe8c1e50acc71ecc237904f8788acef732c3e2f
Cam Crews <camcrews@gmail.com>
2017-09-19 15:55:57 -0400

    Photo Album walk-through

--- week3/objectchallenges/photoalbum.js
+++ week3/objectchallenges/photoalbum.js
@@ -1,19 +1,43 @@
 // Album Constructor
-function Album( ) {
-  // to do: set photos instance attribute to an empty array
+function Album() {
+  // set photos instance attribute to an empty array
+  this.photos = [];
 }
-// to do: Add prototype method for addPhoto( src, location )
+// Add prototype method for addPhoto( src, location )
+Album.prototype.addPhoto = function( src, location ) {
 // addPhoto adds a new photo to the Album.photos array
-    // to do: create a new photo inside of the addPhoto method
-    // to do: add the new photo to the Album.photos array (push?)
-// to do: Add prototype method for listPhotos()
-    // to do: maybe use join(", ") to just output a list of photos as a string
-// to do: Add prototype method for getPhoto(index)
-    // to do: return the photo that is located at the index passed in as an argument
-
+    // create a new photo inside of the addPhoto method
+    let photo = new Photo( src, location );
+    // add the new photo to the Album.photos array (push?)
+    this.photos.push( photo );
+//   this.photos.push( new Photo(src,location));
+    return this; // returning this allows method chaining
+};
+// Add prototype method for listPhotos()
+Album.prototype.listPhotos = function() {
+  // maybe use join(", ") to just output a list of photos as a string
+  return this.photos.join(", ");
+};
+   
+// Add prototype method for getPhoto(index)
+Album.prototype.getPhoto = function(index) {
+    // return the photo that is located at the index passed in as an argument
+    return this.photos[index];
+};
 // Photo Constructor
 function Photo( src, location ) {
-  // to do: store src argument in an attribute on the instance via this
-  // to do: store location argument on the instance too
+  // store src argument in an attribute on the instance via this
+  this.src = src;
+  // store location argument on the instance too
+  this.location = location;
 }
-// to do: [optional] Add prototype method for toString on Photo
+// [optional] Add prototype method for toString on Photo
+Photo.prototype.toString = function(){
+  return `${this.src} (${this.location})`;
+}
+
+//tests 
+const myAlbum = new Album();
+
+myAlbum.addPhoto("dog.jpg","Home").addPhoto("Cat","Hat");
+
