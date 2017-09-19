b29015dda5f25e8550bcd9ce95d2b773add48a26
Cam Crews <camcrews@gmail.com>
2017-09-19 16:57:52 -0400

    Student/Teacher starter

---
+++ week3/objectchallenges/classroom.js
@@ -0,0 +1,23 @@
+/* 
+Create a prototypical Person object. 
+From this object, extend a Teacher object and a Student object.
+Each of these objects should have attributes and methods pertinent 
+to what they describe. Also create a School object that should be 
+able to store instances of students and teachers. Make sure to 
+write code afterwards that creates instances of these objects to 
+make sure that what you've written works well and you're able to 
+store the necessary data in each object.
+*/
+
+// to do: create Person constructor accepting firstName and lastName arguments
+
+// to do: create a Teacher constructor accepting an email address argument
+  // to do: have the Teacher prototype chain to the Person prototype using Object.create()
+
+// to do: create a Student constructor accepting a cohort argument
+  // to do: have the Student prototype chain to the Person prototype using Object.create()
+
+// to do: create a School constructor that initializes a people array which we'll store both students and teachers in
+// to do: add an addPerson method that accepts a student/teacher/person and adds them to the people array
+
+
