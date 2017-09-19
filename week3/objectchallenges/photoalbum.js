71ba25c14abe93086f9ace9788c5e31391a1f406
Cam Crews <camcrews@gmail.com>
2017-09-19 14:48:46 -0400

    Photo Album starter

---
+++ week3/objectchallenges/photoalbum.js
@@ -0,0 +1,19 @@
+// Album Constructor
+function Album( ) {
+  // to do: set photos instance attribute to an empty array
+}
+// to do: Add prototype method for addPhoto( src, location )
+// addPhoto adds a new photo to the Album.photos array
+    // to do: create a new photo inside of the addPhoto method
+    // to do: add the new photo to the Album.photos array (push?)
+// to do: Add prototype method for listPhotos()
+    // to do: maybe use join(", ") to just output a list of photos as a string
+// to do: Add prototype method for getPhoto(index)
+    // to do: return the photo that is located at the index passed in as an argument
+
+// Photo Constructor
+function Photo( src, location ) {
+  // to do: store src argument in an attribute on the instance via this
+  // to do: store location argument on the instance too
+}
+// to do: [optional] Add prototype method for toString on Photo
