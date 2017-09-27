0a279e9c2d46be99268ed67c7345ba6f323b292b
Cam Crews <camcrews@gmail.com>
2017-09-27 16:13:47 -0400

    Cookie exercise 1

--- demos/cookies/main.js
+++ demos/cookies/main.js
@@ -1,3 +1,5 @@
+//
+const DEFAULT_GREETING = "Cookie Experiment";
 let ready = Cookies.get("Ready_for_lunch");
 if( !ready ) {
   Cookies.set("Ready_for_lunch", "false");
@@ -5,8 +7,36 @@ if( !ready ) {
   document.querySelector("#sandwich").classList.add("active");
 }
 document.addEventListener("DOMContentLoaded",function(){
+  // update the welcome message on load
+  updateWelcome();
+  //listen for form submission (just get the first form we find)
+  document.querySelector("form").addEventListener("submit",function(e){
+    e.preventDefault();
+    // save the name submitted
+    let name = document.querySelector("input[name=name]").value;
+    Cookies.set("name", name);
+    updateWelcome(name);
+  });
+  document.querySelector("#clear").addEventListener("click",function(){
+    Cookies.expire("name"); // clear the cookie
+    document.querySelector("h1").innerText = DEFAULT_GREETING;
+  });
+  // when we click on "hangry" button, set cookie to true and make
+  // #sandwich image visible
   document.querySelector("#hangry").addEventListener("click",function(){
     Cookies.set("Ready_for_lunch", "true");
     document.querySelector("#sandwich").classList.add("active");
   });
 });
+
+// function to update the welcome message (if necessary)
+function updateWelcome(new_name){
+  let name = new_name || Cookies.get("name");
+  // if we retrieved a name from the Cookies
+  if( name ) {
+    // update the welcome message
+    document.querySelector("h1").innerText = `Welcome, ${name}!`;
+  }
+}
+
+
