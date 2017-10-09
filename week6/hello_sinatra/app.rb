60afec3058015682e6c43140f267f15f94bdf90d
Cam Crews <camcrews@gmail.com>
2017-10-09 12:30:05 -0400

    Use of templates

--- week6/hello_sinatra/app.rb
+++ week6/hello_sinatra/app.rb
@@ -7,38 +7,11 @@ before do
 end
 
 get '/' do
-  <<-HTMLOUT
-  <!DOCTYPE html>
-  <html>
-  <head>
-    <meta charset="UTF-8">
-    <title>#{@title}</title>
-  </head>
-  <body>
-  <h1>Geo Searcher</h1>
-  <p>Please enter a web address to look for.</p>
-  <form method="post" action="/search">
-  <label for="address">Web Address</label>
-  <input name="address" />
-  </form>
-  </body>
-  </html>
-HTMLOUT
+  erb :home
 end
 
 post '/search' do
-  ip_address = Resolv.getaddress(params[:address])
-  results = Geolocater.geolocate_ip(ip_address)
-  <<-HTMLOUT
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="UTF-8">
-  <title>#{@title}</title>
-</head>
-<body>
-  <p>Your site #{params[:address]} has an IP Address of #{ip_address} and is located in #{results["city"]}, #{results["region_code"]}</p>
-</body>
-</html>
-HTMLOUT
+  @ip_address = Resolv.getaddress(params[:address])
+  @results = Geolocater.geolocate_ip(@ip_address)
+  erb :search
 end
