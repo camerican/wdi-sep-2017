dbf36e4d6767a4f2e900d53e5f7abc636943e4a7
Cam Crews <camcrews@gmail.com>
2017-10-09 11:10:56 -0400

    IP Searching

--- class/app.rb
+++ class/app.rb
@@ -3,6 +3,15 @@ require 'geolocater'
 puts "Please enter a web address to search for"
 
 web_address = gets.strip
-ip_address = Geolocater.ip_lookup(web_address)
+
+result = `ping -c 1 #{web_address}`
+
+p result
+
+ip_address = /\(([^\)]+)\)/.match(result)[1]
+
+p ip_address
+
+# ip_address = Geolocater.ip_lookup(web_address)
 
 p Geolocater.geolocate_ip(ip_address)
