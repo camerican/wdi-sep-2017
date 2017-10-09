97156ff316a3d1173c73fe2d659041c80e83a251
Cam Crews <camcrews@gmail.com>
2017-10-09 13:49:38 -0400

    roster.rb

---
+++ week6/hello_sinatra/roster.rb
@@ -0,0 +1,126 @@
+@roster = [{
+  first: 'Kevin',
+  last: 'Gallagher',
+  photo: 'https://ca.slack-edge.com/T03JJLPSP-U6W4SAPRT-835ed0912220-512',
+  slack: 'kgallagher',
+  github: 'Kgallagher281'
+},
+{
+  first: 'Lisa',
+  last: 'Pegher',
+  photo: 'https://ca.slack-edge.com/T03JJLPSP-U6WT2E4JJ-8f5202eea9c9-512',
+  slack: 'lisamae',
+  github: 'lisa1357mae'
+},
+{
+  first: 'Mahima',
+  last: 'Tuladhar',
+  photo: 'https://ca.slack-edge.com/T03JJLPSP-U6XC01KA8-394be3c3ce97-512',
+  slack: 'mahima',
+  github: 'Mahima820'
+},
+{
+  first: 'Jack',
+  last: 'Floyd',
+  photo: 'https://ca.slack-edge.com/T03JJLPSP-U6VH16CQ1-73d7094a66ba-512',
+  slack: 'mjfloyd3',
+  github: 'mjfloyd3'
+},
+{
+  first: 'Valerie',
+  last: 'Kirby',
+  photo: 'https://ca.slack-edge.com/T03JJLPSP-U6XNWCY6T-c4c69c2d80d6-512',
+  slack: 'vpkirby002',
+  github: 'vpkirby002'
+},
+{
+  first: 'Christy',
+  last: 'Kusuma',
+  photo: 'https://ca.slack-edge.com/T03JJLPSP-U6VBJ2URE-7255eee92e8d-512',
+  slack: 'ckusuma',
+  github: 'christykusuma'
+},
+{
+  first: 'Joan',
+  last: 'Borbon',
+  photo: 'https://ca.slack-edge.com/T03JJLPSP-U6Y9JEA03-ef52bfce8920-512',
+  slack: 'jborbon',
+  github: 'jborbon'
+},
+{
+  first: 'Veronica',
+  last: 'Martinez',
+  photo: 'https://ca.slack-edge.com/T03JJLPSP-U2QPMNQE7-b9b7fd877945-512',
+  slack: 'martinezvea',
+  github: 'martinezvea'
+},
+{
+  first: 'Vikki',
+  last: 'Braxton',
+  photo: 'https://ca.slack-edge.com/T03JJLPSP-U6X55A4LX-8429ebc3bbf6-512',
+  slack: 'vbraxtvi',
+  github: ''
+},
+{
+  first: 'Daniel',
+  last: 'Bobadilla',
+  photo: 'https://ca.slack-edge.com/T03JJLPSP-U6YFN9WER-174ec73e16dc-512',
+  slack: 'daniel1995db',
+  github: 'Daniel1995db'
+},
+{
+  first: 'Mikey',
+  last: 'Lane',
+  photo: 'https://ca.slack-edge.com/T03JJLPSP-U6W6NQZNU-9f74e019eb18-512',
+  slack: 'mikeydlane',
+  github: 'mikeydlane'
+},
+{
+  first: 'Don',
+  last: 'Chiamthamachinda',
+  photo: 'https://ca.slack-edge.com/T03JJLPSP-U6VUTD7UH-647172c98c2b-512',
+  slack: 'don.chiam',
+  github: 'donchiam'
+},
+{
+  first: 'John',
+  last: 'Murphy',
+  photo: 'https://ca.slack-edge.com/T03JJLPSP-U6ZECGYG7-ae94039c103a-512',
+  slack: 'john.Murphy',
+  github: 'jmurphy2404'
+},
+{
+  first: 'Sherill',
+  last: 'Samuel',
+  photo: 'https://ca.slack-edge.com/T03JJLPSP-U6XRR4EM7-bd0547d3f086-512',
+  slack: 'sherill',
+  github: ''
+},
+{
+  first: 'Sophie',
+  last: 'Bushman',
+  photo: 'https://ca.slack-edge.com/T03JJLPSP-U6W4DK8QJ-6f074c9e8a30-512',
+  slack: 'sophiebushman',
+  github: 'sophiebushman'
+},
+{
+  first: 'Michael',
+  last: 'Teixeira',
+  photo: 'https://ca.slack-edge.com/T03JJLPSP-U6WAD2J93-7854ada1c98d-512',
+  slack: 'teixeiramichael5',
+  github: 'MikeTeixeira'
+},
+{
+  first: 'Boony',
+  last: 'Guzman',
+  photo: 'https://ca.slack-edge.com/T03JJLPSP-U6VD69QF2-2e108b45e7f8-512',
+  slack: 'josuegv7',
+  github: 'josuegv7'
+},
+{
+  first: 'Elise',
+  last: 'Barnes',
+  photo: 'https://ca.slack-edge.com/T03JJLPSP-U6W7RCB34-g1a37e409440-512',
+  slack: 'barnese3',
+  github: 'barnese3'
+}]
