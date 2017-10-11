954b4b56c5a1ed94121b0cd5bc7c1f4220ac21b3
Cam Crews <camcrews@gmail.com>
2017-10-11 15:48:57 -0400

    Sendgrid update

--- codewars/README.md
+++ codewars/README.md
@@ -36,5 +36,35 @@ gem 'sendgrid-ruby'
 
 And now we can `bundle install`.  If you don't have bundle recognized, make sure you `gem install bundler`.
 
+## Adding environment variables
 
+### With dotenv
+
+First, add in the dotenv gem to our gemfile:
+
+```
+gem 'dotenv'
+```
+
+and bundle install again.
+
+Next we create a `.env` file by touching it out.  We add in our API key and the value pair into that file:
+
+```
+SENDGRID_API_KEY=1402374247238473024324324
+```
+
+While we're at it, let's add `.env` into .gitignore so that we don't automatically add the `.env` file when we do a `git add .`.
+
+```
+# .gitignore
+.env
+.DS_Store
+```
+
+We added .DS_Store since that's an annoying Mac file that gets everywhere (it's a helper file to load thumbnail images and meta data on folder contents).
+
+We lastly added `require 'dotenv/load'` into our main application.
+
+Now we can access `ENV["SENDGRID_API_KEY"]`
 
--- codewars/app.rb
+++ codewars/app.rb
@@ -1,7 +1,27 @@
+require 'dotenv/load'
+require 'sendgrid-ruby'
 require 'sinatra'
+# require 'eventful_api'
 # to do: require 'sengrid'
+include SendGrid
 
+before do
+  @class = ""
+end
+
+
+# EventfulApi.configure do |config|
+#   config.application_key = ENV['EVENTFUL_API_KEY']
+#   config.consumer_key = ENV['EVENTFUL_OAUTH_KEY']
+#   config.consumer_secret = ENV['EVENTFUL_OAUTH_SECRET']
+# end
+
+
+get '/events' do
+
+end
 get '/' do
+  @class = "home"
   erb :home
 end
 get '/warriors' do
@@ -11,11 +31,26 @@ get '/about' do
   erb :about
 end
 get '/help' do
+  @class = "help"
   erb :help
 end
 post '/help' do
-  params.to_s
+
+  from = Email.new(email: params["email"])
+  to = Email.new(email: 'cam@nycda.com')
+  subject = "Codewars Inquiry (From #{params["name"]})"
+  content = Content.new(type: 'text/plain', value: params["issue"])
+  mail = Mail.new(from, subject, to, content)
+
+  sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+  response = sg.client.mail._('send').post(request_body: mail.to_json)
+  puts response.status_code
+  puts response.body
+  puts response.headers
+
+  redirect '/'
 end
 get '/profile/:id' do
+  @id = params[:id]
   erb :profile
 end
