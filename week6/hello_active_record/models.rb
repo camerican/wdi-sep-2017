eb7478987b5b60d3befbe6f2b21ca74e9f8d9900
Cam Crews <camcrews@gmail.com>
2017-10-13 12:02:58 -0400

    Kevin method

--- week6/hello_active_record/models.rb
+++ week6/hello_active_record/models.rb
@@ -1,6 +1,10 @@
 
 class User < ActiveRecord::Base
   # to do: add in a user method...
+  def kevin
+    self.first + " " + self.last + " (" + self.github + ")"
+  end
 end
 class Payment < ActiveRecord::Base
 end
+
