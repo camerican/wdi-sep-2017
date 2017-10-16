4f2e1c6d994d62a01cc853360dae63e3d7e9feb1
Cam Crews <camcrews@gmail.com>
2017-10-16 14:13:39 -0400

    Login attempt

--- demos/hq_headquarters/app.rb
+++ demos/hq_headquarters/app.rb
@@ -3,7 +3,12 @@ require 'sinatra/activerecord'
 require 'sqlite3'
 require './models'
 
+enable :sessions
 set :database, {adapter: 'sqlite3', database: 'hq.sqlite3'}
+
+before do
+  current_user
+end
 
 get '/' do
   @questions = Question.all
@@ -28,3 +33,36 @@ post '/questions' do
   question.save
   redirect '/'
 end
+
+# handle login -- check if user & pass match
+post '/login' do
+  # first try to find a user with the slack
+  # name provided
+  user = User.find_by(slack: params[:slack])
+  # then, if we did find a user with that slack name,
+  if user && user.password == params[:password]
+    # a-ok!  let them in, and log them in
+    session[:user_id] = user.id
+    redirect '/'
+  else
+    # uh oh, user and pass didn't match
+    redirect :back
+  end
+  # we'll check the password submitted
+  # against the one we have in the DB
+  # if they match, the user is allowed in
+  # and logged in as that user
+  # otherwise, send error message
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect :root
+end
+
+def current_user
+  @current_user = User.find(session[:user_id]) if session[:user_id]
+end
+
+
+
